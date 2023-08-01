bc9047f3df61b4a22e1066c1407eb2e90ad30711
mpericles <mpericles1@yahoo.com>
2023-08-01 12:40:52 -0400

    aspproducts

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+using System;
+
+
+namespace Testing
+{
+    //public interface IProductRepository
+    public interface IProductRepository
+    {
+        
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
